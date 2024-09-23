211d6d1985f5d28a34e8d1689ba3c6350709b27f
dincohen92 <dincohen92@gmail.com>
2024-09-22 21:37:51 -0700

    create work page and set work route to it

---
+++ src/Work.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+
+function Work() {
+  return (
+    <div className="work-page">
+      <Header/>
+      <h1>Recent Works</h1>
+      <div>
+        {/* map projects here */}
+      </div>
+    </div>
+  );
+}
+
+export default Work;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Work from './Work';
 import reportWebVitals from './reportWebVitals';
 import {
   createBrowserRouter,
@@ -19,7 +20,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/work",
-    element: <App/>,
+    element: <Work/>,
   },
   {
     path: "/staff",
