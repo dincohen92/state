94911ce1b3727aeebb5e0df59c0e69ce9c49a154
dincohen92 <dincohen92@gmail.com>
2024-10-20 15:43:24 -0700

    update example data to remove typscript warning on project page. added example data to .gitignore

--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -2,52 +2,52 @@ import Link from "next/link";
 import Image from "next/image";
 import projectData from "../projectData.json"
 
-type Project = {
+interface Project  {
   id: string,
   name: string,
-  date: Date,
+  date: string,
   discipline: string,
   description: string,
-  designers: Designers
-  images: Images
+  designers: Designers[]
+  images: Images[]
   coverImage: string
   tags: string[]
 }
-type Images =   {
+interface Images {
   imageURL: string,
   caption: string
 }
 
-type Designers =   {
+interface Designers {
   name: string,
   role: string
 }
 
-let projects : Project[] = new Project[]
-if (projectData.length){
-  projects = projectData.map((project) =>
-    <div key={project} className="project-card- container">
-      <Link  href={project.id} className="project-card">
-        <Image src={project.images[0].imageURL} alt="project-thumbnail" className="project-thumbnail" fill={true}/>
-        <div className="project-card-details">
-          <div className="project-card-details-header">
-            <h3>{project.name}</h3>
-            <p>{project.date} | {project.discipline}</p>
-          </div>
-          <p className="project-card-details-description">{project.description}</p>
+
+function ProjectPage() {
+  if (projectData.length){
+    return (
+      projectData.map((project: Project) =>
+        <div key={"project"} className="project-card- container">
+          <Link  href={project.id} className="project-card">
+            <Image src={project.images[0].imageURL} alt="project-thumbnail" className="project-thumbnail" width={200} height={200}/>
+            <div className="project-card-details">
+              <div className="project-card-details-header">
+                <h3>{project.name}</h3>
+                <p>{project.date} | {project.discipline}</p>
+              </div>
+              <p className="project-card-details-description">{project.description}</p>
+            </div>
+          </Link>
+          <hr className="horizontal-divider"/>
         </div>
-      </Link>
-      <hr className="horizontal-divider"/>
-    </div>
-  );
+      )
+    );
+  } else {
+    return(
+      <div>Coming Soon</div>
+    );
+  }
 }
 
-function Project() {
-  return (
-    <div>
-      {projects}
-    </div>
-  );
-}
-
-export default Project;
+export default ProjectPage;
