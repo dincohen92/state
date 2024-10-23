254f19c8f30c9ab1c4ae4cd84a6ddf32f761257a
dincohen92 <dincohen92@gmail.com>
2024-10-23 12:45:29 -0700

    move title tag to body to change name of website in browser tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,10 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <title>STATE</title>
-      </Head>
       <body>
+        <title>STATE</title>
         <Header/>
         <div className="mt-16 p-5">
           {children}
